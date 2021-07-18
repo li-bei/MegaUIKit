d98668541113b4028b0cd8ad2650ad2e229bbbad
Bei Li <kylinroc@gmail.com>
2021-07-18 15:20:33 +0800

    Add MUIViewController

---
+++ Sources/MegaUIKit/MUIViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+open class MUIViewController: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
